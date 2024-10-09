f5695bbec4bf7bcdb1082d7e5ba62b449f418fa7
Andrew-Haaksma-students <207887@student.oaisd.org>
2024-10-09 12:25:11 +0000

    Hello

---
+++ Program.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Program{
+    public static void Main(string[] args){
+        string name = Console.ReadLine();
+
+        Console.WriteLine("My friend's name is " + name.);
+    }
+}
