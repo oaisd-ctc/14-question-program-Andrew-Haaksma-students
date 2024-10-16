403f66bb63a62042c031e63f373f252b0968cf5d
Andrew-Haaksma-students <207887@student.oaisd.org>
2024-10-16 13:31:39 +0000

    Organized web page

--- Program.cs
+++ Program.cs
@@ -1,52 +1,115 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿   // See https://aka.ms/new-console-template for more information
 using System;
 
 public class Program
 {
+        public static string name;
+        public static string age;
+        public static string eye;
+        public static string hair;
+        public static string shoe;
+        public static string color;
+        public static string movie;
+        public static string teacher;
+        public static string study;
+        public static string holiday;
+        public static string season;
+        public static string dream;
+        public static string siblings;
+        public static int number;
+
+
+
     public static void Main(string[] args)
     {
-        /*plug in info*/
+        AskName();
+        AskAge();
+        AskEye();
+        AskHair();
+        AskShoe();
+        AskMovie();
+        AskColor();
+        AskTeacher();
+        AskStudy();
+        AskHoliday();
+        AskSeason();
+        AskDream();
+        AskSiblings();
+        AskNumber();
+        InfoText();
+    }
+
+    public static void AskName()
+    {
         Console.WriteLine("enter your name: ");
-        string name = Console.ReadLine();
-        int number = int.Parse(age); 
-
+        name = Console.ReadLine();
+    }
+    public static void AskAge()
+    {
         Console.WriteLine("enter your age: ");
-        string age = Console.ReadLine();
-
+        age = Console.ReadLine();
+    }
+    public static void AskEye()
+    {
         Console.WriteLine("enter your eye color: ");
-        string eye = Console.ReadLine();
-
+        eye = Console.ReadLine();
+    }
+    public static void AskHair()
+    {
         Console.WriteLine("enter your hair color: ");
-        string hair = Console.ReadLine();
-
+        hair = Console.ReadLine();
+    }
+    public static void AskShoe()
+    {
         Console.WriteLine("enter your shoe size: ");
-        string shoe = Console.ReadLine();
-
+        shoe = Console.ReadLine();
+    }
+    public static void AskMovie()
+    {
         Console.WriteLine("enter your favorite movie: ");
-        string movie = Console.ReadLine();
-
+        color = Console.ReadLine();
+    }
+    public static void AskColor()
+    {
         Console.WriteLine("enter your favorite color: ");
-        string color = Console.ReadLine();
-
+        movie = Console.ReadLine();
+    }
+    public static void AskTeacher()
+    {
         Console.WriteLine("enter your favorite teacher: ");
-        string teacher = Console.ReadLine();
-
+        teacher = Console.ReadLine();
+    }
+    public static void AskStudy()
+    {
         Console.WriteLine("enter your favorite class: ");
-        string study = Console.ReadLine();
-
+        study = Console.ReadLine();
+    }
+    public static void AskHoliday()
+    {
         Console.WriteLine("enter your favorite holiday: ");
-        string holiday = Console.ReadLine();
-
+        holiday = Console.ReadLine();
+    }
+    public static void AskSeason()
+    {
         Console.WriteLine("enter your favorite season: ");
-        string season = Console.ReadLine();
-
+        season = Console.ReadLine();
+    }
+    public static void AskDream()
+    {
         Console.WriteLine("enter your dream job: ");
-        string dream = Console.ReadLine();
-
+        dream = Console.ReadLine();
+    }
+    public static void AskSiblings()
+    {
         Console.WriteLine("enter amount of siblings: ");
-        string siblings = Console.ReadLine();
-
-        /*results of info*/
+        siblings = Console.ReadLine();
+    }
+    public static void AskNumber()
+    {
+        number = int.Parse(age);
+    }
+    public static void InfoText()
+    {
         Console.WriteLine("My friend's name is " + name + ". " + name + " is " + age + " years old. " + name + 
         " eye color is " + eye + " with " + hair + " hair and a pair of size " + shoe + " shoes and on them should be " 
         + color + " his favorite color. Which he uses to walk to school with. " + "He also loves the movie " + movie 
@@ -55,4 +118,5 @@ public class Program
         + " Though he does love " + holiday +  " and he does love " + season + ". " + name + " has " + siblings 
         + " sibling with him during his time at highschool and in five years " + name + " will be " + (number + 5) + ".");
     }
+    
 }
